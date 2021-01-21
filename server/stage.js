8821e93d5f7631189fb5f56681c614eb8b482508
David Ammeraal <david.ammeraal@gmail.com>
2021-01-21 13:15:38 +0100

    Updated insync-stage-handler

--- server/stage.js
+++ server/stage.js
@@ -1,43 +1,47 @@
 const path = require("path");
 const fs = require("fs").promises;
 const R = require("ramda");
-const createStageHandler = require("insync-stage-handler");
+const {
+  createStageHandler,
+  createcreateStageSettingsHandler,
+} = require("insync-stage-handler");
 const options = require("./options");
 const { getSettings } = require("./settings");
 const { LOGGER } = require("./logger");
 
 const handler = createStageHandler();
+const settingsHandler = createcreateStageSettingsHandler();
 
 const { assetRoot } = options;
 
-const readStage = file => fs.readFile(file, "utf-8").then(JSON.parse);
+const readStage = (file) => fs.readFile(file, "utf-8").then(JSON.parse);
 
-const joinWithAssetRoot = p => path.normalize(`${assetRoot}/${p}`);
+const joinWithAssetRoot = (p) => path.normalize(`${assetRoot}/${p}`);
 
-const fixPath = stagePath => {
+const fixPath = (stagePath) => {
   const stageDir = path.parse(stagePath).dir;
   return R.evolve({
     path: R.pipe(
-      R.tap(p =>
+      R.tap((p) =>
         LOGGER.debug(`Fixing path "${p}", stage dir = "${stageDir}".`)
       ),
-      R.when(R.complement(path.isAbsolute), p => path.resolve(stageDir, p)),
+      R.when(R.complement(path.isAbsolute), (p) => path.resolve(stageDir, p)),
       R.when(R.complement(R.startsWith(assetRoot)), joinWithAssetRoot),
       path.normalize,
-      p => path.relative(stageDir, p),
+      (p) => path.relative(stageDir, p),
       // TODO: This is a hack until this is resolved: https://github.com/Noterik/marin-stageplayer/issues/127
-      p => (p.match(/.+.(sbf|h5m)$/gim) ? path.resolve(stageDir, p) : p),
-      R.tap(p => LOGGER.debug(`Fixed path "${p}.`))
-    )
+      (p) => (p.match(/.+.(sbf|h5m)$/gim) ? path.resolve(stageDir, p) : p),
+      R.tap((p) => LOGGER.debug(`Fixed path "${p}.`))
+    ),
   });
 };
 
-const getStage = p => {
+const getStage = (p) => {
   const resolvedP = joinWithAssetRoot(p);
   return Promise.all([readStage(resolvedP), getSettings(resolvedP)]).then(
     ([stage, settings]) => {
       return R.pipe(
-        R.mergeLeft(settings),
+        R.mergeLeft(settingsHandler(stage, settings)),
         R.assocPath(["menu", "visible"], false),
         R.assocPath(["annotations", "allowed"], false),
         R.assoc("data", R.prop("data", settings)),
@@ -55,7 +59,7 @@ const getStage = p => {
           R.lensPath(["files", "entities", "files", "byId"]),
           R.map(fixPath(resolvedP))
         ),
-        R.tap(mergedStage => {
+        R.tap((mergedStage) => {
           LOGGER.debug("merged stage = ", mergedStage);
         })
       )(stage);
@@ -64,5 +68,5 @@ const getStage = p => {
 };
 
 module.exports = {
-  getStage
+  getStage,
 };
